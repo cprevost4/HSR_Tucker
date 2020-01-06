1c7d5d87f4b4abf488060f43252770635a1bd335
Clémence <clemence.prevost@univ-lorraine.fr>
2020-01-06 14:40:55 +0100

    Delete all

--- demos/bscott_adaptor.m
+++
@@ -1,5 +0,0 @@
-function [SRI_hat,info] = bscott_adaptor(HSI, MSI, Pm, R, opts)
-%Changes the order of parameters 
-  [SRI_hat,info] = bscott(MSI, HSI, Pm, R, opts);
-end
-
--- demos/scuba_adaptor.m
+++
@@ -1,5 +0,0 @@
-function [SRI_hat,info] = scuba_adaptor(HSI, MSI, Pm, R, opts)
-%Changes the order of parameters 
-  [SRI_hat,info] = scuba(MSI, HSI, Pm, R, opts);
-end
-
